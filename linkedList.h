28fc53bd8cec89602e624692252e54c89326e339
Julian Zarazua <julianzarazua505@gmail.com>
2023-10-24 22:16:43 -0500

    move linkedList implementation to header file

--- linkedList.h
+++ linkedList.h
@@ -14,15 +14,63 @@ class LinearSinglyLinkedList {
     bool isFullList();
     bool addElmAtFront(T);
     bool insertElmAtEnd(T);
-    //bool removefromFront(); WHAT DO THE FOLLOWING METHODS RETURN?
-    //void copyList(); WHAT ARGUMENTS DO THEY ACCEPT?
-    //void deleteList();
-    //void nextElm();
+    bool removefromFront(T);
+    void copyList(); 
+    void deleteList();
+    void nextElm();
     
     private:
     Node<T> *head;
     int maxSize;
     int currentSize;
 };
+
+template <typename T> LinearSinglyLinkedList<T>::LinearSinglyLinkedList() {
+    head = nullptr;
+}
+
+template <typename T> LinearSinglyLinkedList<T>::~LinearSinglyLinkedList() {
+
+}
+
+template <typename T> bool LinearSinglyLinkedList<T>::setMaxSize(int newMaxSize) {
+    maxSize = newMaxSize;
+    return true;
+}
+
+template <typename T> bool LinearSinglyLinkedList<T>::isEmptyList() {
+    return !head;
+}
+
+template <typename T> bool LinearSinglyLinkedList<T>::isFullList() {
+    return maxSize == currentSize;
+}
+
+template <typename T> bool LinearSinglyLinkedList<T>::addElmAtFront(T data) {
+    Node<T> *newNode = new Node<T>{data, head};
+    head = newNode;
+    currentSize++;
+    return true;
+}
+
+template <typename T> bool LinearSinglyLinkedList<T>::insertElmAtEnd(T data) {
+    if (isFullList) {
+        return false;
+    }
+    Node<T> *current = head;
+    Node<T> *previous = nullptr;
+    while (current) {
+        previous = current;
+        current = current->next;
+    }
+    Node<T> *newNode = new Node<T>{data, nullptr};
+    if (previous) {
+        previous->next = newNode;
+    } else {
+        head = newNode;
+    }
+    currentSize++;
+    return true;
+}
 
 #endif
